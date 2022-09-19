ff0526bc7e792c8f49729bb64a250f3d1fab3e21
mark <mark.munene@student.moringaschool.com>
2022-09-19 03:29:29 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(spy){
+    console.log (spy());
+}
+receivesAFunction(function(){
+    return "spy"
+})
+function returnsANamedFunction(){
+return function receivesAFunction(){
+
+}
+}
+
+function  returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
